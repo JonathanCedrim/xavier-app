db9f775becf63ad51db6b0ded5c5bbae85833891
JonathanCedrim <jonathan.cedrim.souza@gmail.com>
2018-01-06 21:25:59 -0200

    default crud, module e depedencias

---
+++ src/app/models/movimento/movimento-form/movimento-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movimento-form',
+  templateUrl: './movimento-form.component.html',
+  styleUrls: ['./movimento-form.component.css']
+})
+export class MovimentoFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
