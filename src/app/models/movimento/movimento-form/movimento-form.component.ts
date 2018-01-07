1abf6ce1f90960a5c440fc8a3e0fb91df4148ada
JonathanCedrim <jonathan.cedrim.souza@gmail.com>
2018-01-07 09:01:32 -0200

    movimento: busca por codigo e alteração no formulario

--- src/app/models/movimento/movimento-form/movimento-form.component.ts
+++ src/app/models/movimento/movimento-form/movimento-form.component.ts
@@ -4,10 +4,10 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 
 import { Movimento } from '../shared/movimento';
-import { Cep } from '../../../shared/cep';
-import { CepService } from '../../../shared/cep.service';
 import { MovimentoService } from '../shared/movimento.service';
 import { BasicValidators } from '../../../shared/basic-validators';
+import { VendedorService } from '../../vendedor/shared/vendedor.service';
+import { ClienteService } from '../../cliente/shared/cliente.service';
 
 @Component({
   selector: 'app-movimento-form',
@@ -28,13 +28,17 @@ export class MovimentoFormComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private movimentoService: MovimentoService,
-    private cepService: CepService
+    private vendedorService: VendedorService,
+    private clienteService: ClienteService
   ) {
       this.firstFormGroup = formBuilder.group({
           id:['', [Validators.minLength(0)]],
           codigo: [''],
           vendedor: ['', [Validators.minLength(0)]],
-          cliente: ['', [Validators.minLength(0)]],
+          clienteCodigo: ['', [Validators.minLength(0)]],
+          clienteNome: ['', [Validators.minLength(0)]],
+          vendedorCodigo: [''],
+          vendedorNome: ['', [Validators.minLength(0)]],
           dataCadastro: [''],
           numeroRecibo: ['', [Validators.min(2), Validators.maxLength(70)]],
           numeroPedido: ['', [Validators.min(2), Validators.maxLength(70)]],
@@ -56,13 +60,19 @@ export class MovimentoFormComponent implements OnInit {
     this.id = id ? id: null;
 
     if(!id) {
+      this.movimento.dataCadastro = new Date();      
       this.codigoWrite = true;
       return ;}
 
     this.movimentoService.getMovimento(id)
       .subscribe(
         movimento => {
-          this.movimento = movimento;           
+          this.movimento = movimento;
+              if(this.movimento.dataPagamento != null || this.movimento.dataPagamento != undefined)
+              {
+                this.movimento.dataCadastro = new Date(this.movimento.dataCadastro);
+              }              
+
               if(this.movimento.dataPagamento != null || this.movimento.dataPagamento != undefined)
               {
                 this.movimento.dataPagamento = new Date(this.movimento.dataPagamento);
@@ -95,5 +105,19 @@ export class MovimentoFormComponent implements OnInit {
     }
 
     resultado.subscribe(data => this.router.navigate(['movimento']));
+  }
+
+  atualizaCamposVendedor(codigo) {
+    this.vendedorService.getVendedorByCodigo(codigo)
+      .subscribe(data => {
+        this.movimento.vendedor = data;
+      });
+  }
+
+  atualizaCamposCliente(codigo) {
+    this.clienteService.getClienteByCodigo(codigo)
+      .subscribe(data => {
+          this.movimento.cliente = data;
+      });
   }
 }
